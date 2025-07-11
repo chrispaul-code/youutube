04b95e9e2692639e09bcb97ec5c83e5629f11e96
Christeen <chrispaul1311@gmail.com>
2025-07-11 20:26:14 +0530

    hidding-api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
-const GOOGLE_API_KEY="AIzaSyBEYILjMY-wSDbZYIvKvIfsxVNLLD2icFQ";
+require('dotenv').config();
+
+const apiKey = process.env.GOOGLE_API_KEY;
 
 export const YOUTUBE_VIDEO_API = 
   'https://youtube.googleapis.com/youtube/v3/videos?' +
@@ -8,7 +10,7 @@ export const YOUTUBE_VIDEO_API =
   '&regionCode=IN' +
   '&videoCategoryId=10' +
   '&videoDuration=long' + 
-  '&key=' + GOOGLE_API_KEY;
+  '&key=' + apiKey;
 
 
 export const YOUTUBE_LIKED="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&myRating=like&key="+GOOGLE_API_KEY;
